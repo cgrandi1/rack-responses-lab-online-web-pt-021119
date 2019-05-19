b9a77a3e8e764df46870eb449d3dcac79b5583d8
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-19 18:41:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(evn)
     resp = Rack::Response.new
 
-    time = Time.now.hour
+    time = Time.now.hour(0...23.59)
     morning = Time.now.hour(0...11.59)
-    
+
 
 
     resp.write "#{time}"
