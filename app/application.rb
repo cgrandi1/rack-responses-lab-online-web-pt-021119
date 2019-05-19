23ec5f1cd02f3af8bd5bf1a8364a32162e97d8c6
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-19 18:22:03 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  def call(evn)
+    resp = Rack::Response.new
+
+    time = Kernal.rand(0...23.59)
+    morning = Kernal.rand(0...11.59)
+    afternoon = Kernal.rand(12...23.59)
+
+    if time == 0...11.59 
+      resp.write "Good morning"
+    else 
+      resp.write "Good Afternoon"
+    end 
+
+    resp.finish
+end
